07e038b089df4e0bae243a31e1e908de350583fc
rremizov <rremizov@yandex.ru>
2021-05-20 17:26:31 +0500

    Split migrations

--- migrations/3_deploy_pancake_router_mock.js
+++ migrations/3_deploy_pancake_router_mock.js
@@ -1,9 +1,8 @@
-const Buyer = artifacts.require('Buyer')
+const PancakeRouterMock = artifacts.require('PancakeRouterMock')
 
 module.exports = async function (deployer, network, accounts) {
   if (network === 'kovan' || network === 'kovan-fork') {
     weth = '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
-    pancakeRouterMock = '0x32e0b57758Da892dC2389241902923fEA6d990f6'
-    await deployer.deploy(Buyer, weth, pancakeRouterMock);
+    await deployer.deploy(PancakeRouterMock, weth);
   }
 }
