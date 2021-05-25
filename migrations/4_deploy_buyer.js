3219bd68372a9b590a4dc292bf06970862aaec94
rremizov <rremizov@yandex.ru>
2021-05-25 13:07:49 +0500

    Link Buyer with Uniswap on Kovan

--- migrations/4_deploy_buyer.js
+++ migrations/4_deploy_buyer.js
@@ -3,6 +3,8 @@ const PancakeRouterMock = artifacts.require('PancakeRouterMock')
 
 module.exports = async function (deployer, network, accounts) {
   if (network === 'kovan' || network === 'kovan-fork') {
-    await deployer.deploy(Buyer, PancakeRouterMock.address);
+    // await deployer.deploy(Buyer, PancakeRouterMock.address);
+    const uniswap = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+    await deployer.deploy(Buyer, uniswap);
   }
 }
